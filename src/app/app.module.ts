77a69cc44c1346b5ae50cba2b0e5408d8095236d
akhil <akhil.paul@ymail.com>
2022-11-25 17:07:51 +0530

    Adds add course feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes = [{path:"",component:AddComponent},
+                        {path:"add",component:AddComponent},
+                      ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
